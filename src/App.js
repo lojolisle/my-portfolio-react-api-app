94b1892eb41618c6c67ac2d736a3b0d594f537ac
Lisle Lojo <lislelojo21@gmail.com>
2022-04-19 21:50:10 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import api from './api/portfolio';
+import { useState, useEffect } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [skills, setSkills] = useState([]);
+  const [work, setWork] = useState([]);
+  const [user, setUser] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await api.get('/skills');
+      // const response = await api.get('/projects/list')
+        if(response && response.data) setSkills(response.data);
+        console.log('-------', response.data)
+        console.log('--ss--', skills)
+        if (response.status !== 200) throw Error('SOmething went wrong! No data received from API call');
+        setFetchError(null);
+      } catch(err) {
+          if (err.response) {
+            // not ok (not in 200 response range satus code)
+            console.log('err-',err.response.data);
+            console.log(err.response.status);
+            setFetchError(err.message);
+          } else {
+            console.log(`Error: ${err.message}`);
+            setFetchError(err.message);
+          }
+      }
+    }
+    fetchSkills();
+  }, [])
+
+  useEffect(() => {
+    const fetchWorks = async () => {
+      try {
+        const workResponse = await api.get('/projects');
+        if(workResponse && workResponse.data) setWork(workResponse.data);
+        console.log('-------', workResponse.data)
+        console.log('--work--', work)
+        
+        if (workResponse.status !== 200) throw Error('SOmething went wrong! No data received from API call');
+        setFetchError(null);
+      } catch(err) {
+          if (err.response) {
+            // not ok (not in 200 response range satus code)
+            console.log(err.response.data);
+            console.log(err.response.status);
+            setFetchError(err.message);
+          } else {
+            console.log(`Error: ${err.message}`);
+            setFetchError(err.message);
+          }
+      }
+    }
+    fetchWorks();
+  }, [])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userResponse = await api.get('/user');
+        if(userResponse && userResponse.data) setUser(userResponse.data[0]);
+        console.log('--uset-----', userResponse.data[0])
+        console.log('--user--', user)
+        console.log('[[[[[[', userResponse)
+        if (userResponse.status !== 200) throw Error('SOmething went wrong! No data received from API call');
+        setFetchError(null);
+      } catch(err) {
+          if (err.response) {
+            // not ok (not in 200 response range satus code)
+            console.log(err.response.data);
+            console.log(err.response.status);
+            setFetchError(err.message);
+          } else {
+            console.log(`Error: ${err.message}`);
+            setFetchError(err.message);
+          }
+      }
+    }
+    fetchUser();
+  }, [])
+
+    return (
+      <div>
+        <Navbar />
+        <Home user={user} />
+        <About user={user} />
+        <Skills skills={skills} />
+        <Work work={work} />
+        <Contact />
+      </div>
+    );
 }
 
 export default App;
