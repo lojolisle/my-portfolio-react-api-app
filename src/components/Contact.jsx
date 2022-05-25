24502aace2da0d8c68715233ff447dfab1e3188f
lojolisle <llojo@LAPTOP-FOPF8RGF>
2022-05-24 22:59:58 -0400

    additional style fixes for mac and windows

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
 const Contact = () => {
 
   return (
-    <div name='contact' className='w-full max-h-full bg-[#0a192f] flex justify-center items-center p-4 pt-12'>
+    <div name='contact' className='w-full  h-screen max-h-full bg-[#0a192f] flex justify-center items-center p-4 pt-16'>
         <form method='POST' action="https://getform.io/f/03d68f18-57ab-4aa4-bcde-8a995c61b208" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
+            <div className='pb-8 pt-24'>
                 <p className='text-4xl font-bold inline border-b-4 border-amber-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'> Submit the form below or shoot me an email - lislelojo21@gmail.com</p>
             </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,10 +4,10 @@ import Skill from '../components/skill';
 const Skills = ({ skills }) => {
 
   return (    
-    <div name='skills' className='w-full max-h-full bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full 2xl:h-screen xl:h-full lg:h-screen max-h-full bg-[#0a192f] text-gray-300'>
       {/* Start Container */}
      
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-[1000px] mx-auto p-4 pt-20 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-amber-600 '>Skills</p>
               <p className='py-4'>These are few of the technologies I have worked with</p>
